45439feeb8c572569d2d052f0c398fcd4a858195
LucasNB <rlucasnickbryan@gmail.com>
2023-01-23 18:29:27 +0300

    create: note model

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var noteModel = require('./models/note.model')
+app.use(noteModel)
 var app = express();
 
 // view engine setup
@@ -21,6 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+const db = require("./models/note.model");
+db.sequelize.sync()
+    .then(() => {
+      console.log("Synced db.");
+    })
+    .catch((err) => {
+      console.log("Failed to sync db: " + err.message);
+    });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/note.model.js
@@ -0,0 +1,74 @@
+const {Sequelize, DataTypes} = require("sequelize");
+const sequelize = new Sequelize('./../configuration/database');
+
+const Note = sequelize.define("note", {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    mention: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    parcours: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    level: { // L1, ..., M2,...
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    subject: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    session: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    numbers: {  // numbers of papers
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    sending: { // way to send notes ex:colis ...
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    univ_year: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    operation_date: {
+        type: DataTypes.DATE,
+        defaultValue: new Date(),
+    },
+
+    comment: {
+        type: DataTypes.TEXT
+    },
+
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //
+    //   references: {
+    //     // This is a reference to another model
+    //     model: Bar,
+    //
+    //     // This is the column name of the referenced model
+    //     key: 'id',
+    //
+    //     // With PostgreSQL, it is optionally possible to declare when to check the foreign key constraint, passing the Deferrable type.
+    //     deferrable: Deferrable.INITIALLY_IMMEDIATE
+    //   }
+    // }
+});
+
+(async () => {
+    await sequelize.sync({force: true});
+    // Code here
+})();
+
+module.exports = Note;
