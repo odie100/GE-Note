c7c77bc476c6dee2657cf7204940d8c648638e18
odie100 <andriampeno.odilon@gmail.com>
2023-01-23 15:38:38 +0300

    feat: env file for environment variables fix: changing mysql dependencies to sequelize

--- app.js
+++ app.js
@@ -38,9 +38,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = 5000
-app.listen(port, () => {
-  console.log("Listening on port "+ port)
-});
 
 module.exports = app;
